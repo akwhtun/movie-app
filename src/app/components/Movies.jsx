8ab0a8834517aa4af2d3113cc6d5917c42d6b18e
Aung Kyaw Wai Htun <105596104+akwhtun@users.noreply.github.com>
2024-06-30 02:09:22 +0630

    complete add loading skeleton

--- src/app/components/Movies.jsx
+++ src/app/components/Movies.jsx
@@ -4,67 +4,105 @@ import Link from 'next/link';
 import { RiArrowRightDoubleLine } from "react-icons/ri";
 import { RiArrowLeftDoubleFill } from "react-icons/ri";
 import { useSearchParams } from 'next/navigation';
-
+import { useContext } from "react";
+import ThemeContext from "@/context/ThemeContext";
+import Skeleton, { SkeletonTheme } from 'react-loading-skeleton';
+import 'react-loading-skeleton/dist/skeleton.css';
 export default function Movies() {
     const searchParams = useSearchParams();
     const searchPage = parseInt(searchParams.get("page")) || 1;
     const [movies, setMovies] = useState([]);
     const [page, setPage] = useState(searchPage);
     const [totalPage, setTotalPage] = useState(1);
+    const [loading, setLoading] = useState(true);
 
+    const { theme, setTheme } = useContext(ThemeContext);
+
+    const loadingArray = [1, 2, 3, 4, 5, 6, 7, 8]
     useEffect(() => {
         setPage(searchPage);
     }, [searchPage]);
 
     useEffect(() => {
         const fetchMovies = async () => {
-            const API_KEY = process.env.NEXT_PUBLIC_TMDB_API_KEY;
-            const response = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&page=${page}`);
-            const data = await response.json();
-            const moviesData = data.results;
-            let moviesTotalPage = data.total_pages;
+            try {
+                setLoading(true);
+                const API_KEY = process.env.NEXT_PUBLIC_TMDB_API_KEY;
+                const response = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&page=${page}`);
+                const data = await response.json();
+                const moviesData = data.results;
+                let moviesTotalPage = data.total_pages;
 
-            const MAX_PAGE_LIMIT = 500;
-            if (moviesTotalPage > MAX_PAGE_LIMIT) {
-                moviesTotalPage = MAX_PAGE_LIMIT;
+                const MAX_PAGE_LIMIT = 500;
+                if (moviesTotalPage > MAX_PAGE_LIMIT) {
+                    moviesTotalPage = MAX_PAGE_LIMIT;
+                }
+
+                setMovies(moviesData);
+                setTotalPage(moviesTotalPage);
+                setLoading(false);
+            } catch {
+                setLoading(false);
             }
-
-            setMovies(moviesData);
-            setTotalPage(moviesTotalPage);
-
         };
         fetchMovies();
     }, [page]);
 
-    const handleNextPage = () => {
-        setPage(prevPage => Math.min(prevPage + 1, totalPage));
-    };
+    if (loading) {
+        return (
 
-    const handlePrevPage = () => {
-        setPage(prevPage => Math.max(prevPage - 1, 1));
-    };
+
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 mt-4'>
+                {
+                    loadingArray.map(arr => (
+                        <>
+                            <div className={`p-4 shadow-lg rounded-lg border ${theme === 'dark' ? 'border-gray-200' : 'border-gray-800'}`}>
+                                <SkeletonTheme baseColor={theme === 'dark' ? '#202020' : '#ebebeb'} highlightColor={theme === 'dark' ? '#444' : '#f5f5f5'}>
+                                    <Skeleton height={400} />
+                                    <Skeleton height={40} count={3} />
+                                </SkeletonTheme>
+                            </div>
+                        </>
+                    ))
+                }
+            </div>
+
+
+        )
+    }
 
     return (
         <div>
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 mt-4">
                 {movies.map(movie => (
-                    <MoviesList key={movie.id} {...movie} />
+                    <MoviesList key={movie.id} {...movie} loading={loading} />
                 ))}
             </div>
             <div className="flex justify-center items-center mt-4">
-                <Link href={`/?page=${page > 1 ? page - 1 : 1}`} className={`px-4 py-2 m-2 ${page === 1 ? 'bg-gray-300 text-gray-600' : 'bg-blue-500 text-white'} rounded flex`}>
+                <Link
+                    href={`/?page=${page > 1 ? page - 1 : 1}`}
+                    className={`px-4 py-2 m-2 ${page === 1 ? 'bg-gray-300 text-gray-600' : 'bg-blue-500 text-white'} rounded flex`}
+                >
                     <RiArrowLeftDoubleFill className='text-2xl' />
                     <p className='md:block hidden'>Previous</p>
                 </Link>
-                <Link href={`/?page=1`} className={`px-4 py-2 m-2 ${searchPage == 1 ?
-                    'bg-gray-300 text-black' : 'bg-blue-500 text-white'}  rounded`}>
+                <Link
+                    href={`/?page=1`}
+                    className={`px-4 py-2 m-2 ${searchPage === 1 ? 'bg-gray-300 text-black' : 'bg-blue-500 text-white'} rounded`}
+                >
                     1
                 </Link>
                 <p className='text-3xl px-4 py-2'>.....</p>
-                <Link href={`/?page=${totalPage}`} className={`px-4 py-2 m-2 ${searchPage == totalPage ? 'bg-gray-300 text-black' : 'bg-blue-500 text-white'}rounded`}>
+                <Link
+                    href={`/?page=${totalPage}`}
+                    className={`px-4 py-2 m-2 ${searchPage === totalPage ? 'bg-gray-300 text-black' : 'bg-blue-500 text-white'} rounded`}
+                >
                     {totalPage}
                 </Link>
-                <Link href={`/?page=${totalPage > page ? page + 1 : page}`} className={`px-4 py-2 m-2 ${page === totalPage ? 'bg-gray-300 text-gray-600' : 'bg-blue-500 text-white'} rounded flex`}>
+                <Link
+                    href={`/?page=${totalPage > page ? page + 1 : page}`}
+                    className={`px-4 py-2 m-2 ${page === totalPage ? 'bg-gray-300 text-gray-600' : 'bg-blue-500 text-white'} rounded flex`}
+                >
                     <p className='md:block hidden'>Next</p>
                     <RiArrowRightDoubleLine className='text-2xl' />
                 </Link>
--- src/app/components/MoviesList.jsx
+++ src/app/components/MoviesList.jsx
@@ -1,15 +1,32 @@
-import { useContext } from "react";
-import ThemeContext from "@/context/ThemeContext"
+import { useContext, useState } from "react";
+import ThemeContext from "@/context/ThemeContext";
+import Skeleton, { SkeletonTheme } from 'react-loading-skeleton';
+import 'react-loading-skeleton/dist/skeleton.css';
 
 export default function MoviesList({ backdrop_path, id, original_language, original_title, overview, poster_path, release_date, vote_average, vote_count }) {
-  const {theme, setTheme} = useContext(ThemeContext);
+
+
+  const { theme, setTheme } = useContext(ThemeContext);
   return (
     <div className={`p-4 shadow-lg rounded-lg border ${theme === 'dark' ? 'border-gray-200' : 'border-gray-800'}`}>
-      <img src={`https://image.tmdb.org/t/p/w500${poster_path}`} alt={original_title} className="w-full h-64 object-cover rounded-t-lg" />
+
+      <img
+        src={`https://image.tmdb.org/t/p/w500${poster_path}`}
+        alt={original_title}
+        className="w-full h-64 object-cover rounded-t-lg"
+      />
+
       <div className={`p-4 relative lg:h-48 ${theme === 'dark' ? 'text-gray-300' : 'text-gray-900'}`}>
+
         <h3 className="text-xl font-semibold mb-2 lg:h-14 lg:overflow-hidden">{original_title}</h3>
-        <p className="text-sm  mb-2">Rating: {vote_average} ({vote_count} votes)</p>
-        <p className=" mb-2 line-clamp-2">{overview}</p>
+
+
+
+        <p className="text-sm mb-2">Rating: {vote_average} ({vote_count} votes)</p>
+
+
+
+        <p className="mb-2 line-clamp-2">{overview}</p>
         <a href={`https://www.themoviedb.org/movie/${id}`} target="_blank" rel="noopener noreferrer" className="text-blue-500 hover:underline absolute bottom-0">
           View Details
         </a>
